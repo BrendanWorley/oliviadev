d689208d49a10c6b111b44468e473b23eefeb083
BrendanWorley <texas-triumph@protonmail.com>
2022-10-04 14:48:51 +0300

    commited on 04oct2022 - more of less finished with header and the menu. Time to get busy with first section

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ import { mult, sum } from './modules/calc';
 
 // header menu operation-------------------------------------------------
 visualViewport.addEventListener('resize', recizeCheck);
-// window.addEventListener('resize', checkWinRatio);
+
+window.addEventListener('resize', lowCheck);
 
 const burger = document.querySelector('.header__burger');
 const headerMenu = document.querySelector('.header__menu');
 
 const header = document.querySelector('.header');
-
 
 
 let menuStatus = 'closed';
@@ -40,7 +40,7 @@ function burgerOps() {
             document.querySelector('#fourth').style.visibility = 'hidden';
             document.querySelector('#second').style.transform = 'translateY(5px) rotate(-45deg)';
             document.querySelector('#third').style.transform = 'rotate(45deg) translateX(-2px) translateY(2px)';
-                    
+                   
             } else if (menuStatus === "opened") {
             headerMenu.style.transform = 'translateY(-500%)';
             headerMenu.style.transition = 'all 1.5s ease-in-out'; //
@@ -105,6 +105,24 @@ function recizeCheck() {
 //         headerMenu.style.height = '400px';
 //     } 
 // }
+
+function lowCheck() {
+    if (visualViewport.width < 781 && visualViewport.height < 400) {
+        header.classList.add('_low');
+        headerMenu.style.marginTop = '-22px';
+        headerMenu.style.height = 'calc(100vh - 55px + 22px)';
+        
+    } else if ((visualViewport.height > 400 && visualViewport.width > 450) || visualViewport.width > 781) {
+        header.classList.remove('_low');
+        headerMenu.style.marginTop = '0';
+        headerMenu.style.height = '';
+        
+    } else if (visualViewport.width < 450 && visualViewport.height > 400) {
+        header.classList.remove('_low');
+        headerMenu.style.marginTop = '0';
+        headerMenu.style.height = '';
+    }  
+}
 
 
 // burger animation --------------------------------------------------------------
